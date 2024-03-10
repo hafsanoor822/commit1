e375c70eb093cab2ccb04578725342ee60174f0f
hafsanoor822 <hmamzsis5@gmail.com>
2024-03-10 22:16:23 +0500

    1st commit

---
+++ day1.js
@@ -0,0 +1,34 @@
+"use strict";
+//print message
+console.log("Hey Abu Hurairah! I have started learning Typescript");
+//store name and print
+var myName = "Hafsa Noor";
+console.log(myName);
+//store 10 numbers and add,subtract,multiply and display the result
+var num1 = 1;
+var num2 = 3;
+var num3 = 5;
+var num4 = 7;
+var num5 = 9;
+var num6 = 10;
+var num7 = 11;
+var num8 = 12;
+var num9 = 13;
+var num10 = 14;
+//sum
+var sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+console.log("sum of numbers=", sum);
+//difference
+var diff = num10 - num4;
+console.log("the difference between two numbers is=", diff);
+//product
+var product = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10;
+console.log("product of numbers=", product);
+//take two numbers and display their division
+var numb1 = 6;
+var numb2 = 2;
+var division = numb1 / numb2;
+console.log("division of numbers=", division);
+//all operations
+var result = num2 + num9 / num7 * num4 - num6;
+console.log("The result after all the operations=", result);
---
+++ day1.ts
@@ -0,0 +1,37 @@
+//print message
+console.log("Hey Abu Hurairah! I have started learning Typescript");
+
+//store name and print
+var myName:string="Hafsa Noor";
+console.log(myName);
+
+//store 10 numbers and add,subtract,multiply and display the result
+var num1:number=1
+var num2:number=3
+var num3:number=5
+var num4:number=7
+var num5:number=9
+var num6:number=10
+var num7:number=11
+var num8:number=12
+var num9:number=13
+var num10:number=14
+//sum
+var sum:number=num1+num2+num3+num4+num5+num6+num7+num8+num9+num10
+console.log("sum of numbers=",sum)
+//difference
+var diff:number=num10-num4
+console.log("the difference between two numbers is=",diff)
+//product
+var product:number=num1*num2*num3*num4*num5*num6*num7*num8*num9*num10
+console.log("product of numbers=",product)
+
+//take two numbers and display their division
+var numb1:number=6
+var numb2:number=2
+var division:number=numb1/numb2
+console.log("division of numbers=",division)
+
+//all operations
+var result:number=num2+num9/num7*num4-num6
+console.log("The result after all the operations=",result)
